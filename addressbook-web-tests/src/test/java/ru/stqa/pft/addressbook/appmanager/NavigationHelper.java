74140bddb393962e7312ea07e5020f9a1a76f879
atalaman <a.talaman@gmail.com>
2016-02-29 15:17:22 -0500

    Class HelperBase has been created, filling forms methods has been optimized

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NavigationHelper extends HelperBase {
+  private FirefoxDriver wd;
+
+  public NavigationHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+
+  public void gotoGroupPage() {
+    click(By.linkText("groups"));
+  }
+
+  public void gotoNewContactPage() {
+    click(By.linkText("add new"));
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -2,46 +2,23 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 
-import org.openqa.selenium.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() {
-        gotoNewContactPage();
-        fillContactform(new ContactData("Ivan", "Petrov", "Somewhere", "+19012345678", "ipetrov@gmail.com"));
-        submitContactCreation();
-        returnToHomePage();
+        app.getNavigationHelper().gotoNewContactPage();
+        app.getContactHelper().fillContactform(new ContactData("Ivan", "Petrov", "Somewhere", "+19012345678", "ipetrov@gmail.com"));
+        app.getContactHelper().submitContactCreation();
+        app.getContactHelper().returnToHomePage();
     }
 
-    public void returnToHomePage() {
-        app.wd.findElement(By.linkText("home page")).click();
-    }
 
-    public void submitContactCreation() {
-        app.wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    }
 
-    private void fillContactform(ContactData contactData) {
-        app.wd.findElement(By.name("firstname")).click();
-        app.wd.findElement(By.name("firstname")).clear();
-        app.wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-        app.wd.findElement(By.name("lastname")).click();
-        app.wd.findElement(By.name("lastname")).clear();
-        app.wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-        app.wd.findElement(By.name("address")).click();
-        app.wd.findElement(By.name("address")).clear();
-        app.wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        app.wd.findElement(By.name("mobile")).click();
-        app.wd.findElement(By.name("mobile")).clear();
-        app.wd.findElement(By.name("mobile")).sendKeys(contactData.getPhone());
-        app.wd.findElement(By.name("email")).click();
-        app.wd.findElement(By.name("email")).clear();
-        app.wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    }
 
-    public void gotoNewContactPage() {
-        app.wd.findElement(By.linkText("add new")).click();
-    }
+
+
+
+
 }
