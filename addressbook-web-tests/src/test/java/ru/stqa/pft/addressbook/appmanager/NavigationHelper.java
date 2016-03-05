61be5352124b64c68d381573d3fc772930804683
atalaman <a.talaman@gmail.com>
2016-03-04 22:23:33 -0500

    NavigationHelper methods have been updated in order not to do unneseccary  transfers

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -5,21 +5,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase {
-  private WebDriver wd;
 
   public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void gotoGroupPage() {
-    click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1"))
+            && (wd.findElement(By.tagName("h1")).getText().equals("Groups"))
+            && isElementPresent(By.name("new"))) {
+      return;
+      }
+      click(By.linkText("groups"));
   }
 
+
   public void gotoNewContactPage() {
-    click(By.linkText("add new"));
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().contains("add address book entry")) {
+      return;
+    }
+      click(By.linkText("add new"));
   }
 
   public void gotoHomePage() {
+    if(isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 }
